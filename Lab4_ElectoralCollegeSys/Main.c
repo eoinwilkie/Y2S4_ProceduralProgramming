176494f486bfb53185f9865ac4a5ec46dba6164a
Eoin <G00363134@gmit.ie>
2019-02-08 22:05:40 +0000

    +created with arrays, must convert to malloc()

--- Lab4_ElectoralCollegeSys/Main.c
+++ Lab4_ElectoralCollegeSys/Main.c
@@ -6,23 +6,31 @@
 
 void getVoteResults(int votes[][NO_STATES]);
 void displayVoteResults(int votes[][NO_STATES]);
-void displayStateWinner(int votes[][NO_STATES]);
-void displayNewMayor(int votes[][NO_STATES]);
+void displayStateWinners(int votes[][NO_STATES], int stateWinners[NO_STATES]);
+int getStateWinner(int votes[][NO_STATES], int state);
+void displayNewMayor(int stateReults[NO_STATES]);
 char displayCandidate(int candidateNo);
 
 void main()
 {
 	int votes[NO_CANDIDATES][NO_STATES];
+	int stateResults[NO_STATES];		//contains id of winner per state
+	for (int state = 0; state < NO_STATES; state++)
+	{
+		stateResults[state] = 0;
+	}
 	//int **votes;
 	//votes = (int**) malloc()
 
 	getVoteResults(votes);
 	displayVoteResults(votes);
-	displayStateWinner(votes);
-	displayNewMayor(votes);
-}
+	displayStateWinners(votes, stateResults);
+	displayNewMayor(stateResults);
+}//main()
 
-//functions
+/*
+Prompts user to enter values for election table
+*/
 void getVoteResults(int votes[][NO_STATES])
 {
 	for (int state = 0; state < NO_STATES; state++)
@@ -35,9 +43,11 @@ void getVoteResults(int votes[][NO_STATES])
 			scanf("%d", &votes[candidate][state]);
 		}
 	}
+}//getVoteResults()
 
-}
-
+/*
+Displays vote results as table
+*/
 void displayVoteResults(int votes[][NO_STATES])
 {
 	printf("\n\t\t\tTable of the vote data\nState\tCandidate A\tCandidate B\tCandidate C\tCandidate D\n");
@@ -50,72 +60,67 @@ void displayVoteResults(int votes[][NO_STATES])
 		}
 		printf("\n");
 	}
-}
+}//displayVoteResults()
 
-void displayStateWinner(int votes[][NO_STATES])
+/*
+Displays winners for each state and prints to console.
+*/
+void displayStateWinners(int votes[][NO_STATES], int stateWinners[NO_STATES])
+{
+	int stateWinner;
+	printf("Displaying state winners...\n");
+	for (int state = 0; state < NO_STATES; state++)
+	{
+		stateWinner = getStateWinner(votes, state);
+		stateWinners[stateWinner]++;
+		printf("State %d: Candidate %c\n", state+1, displayCandidate(stateWinner));
+	}
+}//displayStateWinners()
+
+/*
+Gets the state winner for each state
+*/
+int getStateWinner(int votes[][NO_STATES], int state)
 {
 	int stateWin;
 	char stateWinChar;
-	int stateHigh;
-
-	printf("\n\t\t\tState winners\n");
-	for (int state = 0; state < NO_STATES; state++)
-	{
-		int stateHigh = -1;
-		for (int candidate = 0; candidate < NO_CANDIDATES; candidate++)
-		{
-			if (votes[candidate][state] > stateHigh)
-			{
-				stateHigh = votes[candidate][state];
-				stateWin = candidate;
-			}
-		}
-		//print state winner
-		stateWinChar = displayCandidate(stateWin);
-
-		printf("State %d winner: %c\n", state+1, stateWinChar);
-	}
-
-}
-
-void displayNewMayor(int votes[][NO_STATES])
-{
-	int statesWon[NO_CANDIDATES];
-
-	int stateWin;
-	char stateWinChar;
-	int stateHigh;
-	int newMayor;
-	char newMayorChar;
-	int highStateWon = 0;
-
-	for (int state = 0; state < NO_STATES; state++)
-	{
-		int stateHigh = -1;
-		for (int candidate = 0; candidate < NO_CANDIDATES; candidate++)
-		{
-			if (votes[candidate][state] > stateHigh)
-			{
-				stateHigh = votes[candidate][state];
-				stateWin = candidate;
-			}
-		}
-		statesWon[stateWin++];
-	}
+	int stateHigh = -1;
 
 	for (int candidate = 0; candidate < NO_CANDIDATES; candidate++)
 	{
-
-		if (statesWon[candidate] > highStateWon)
+		if (votes[candidate][state] > stateHigh)
 		{
-			highStateWon = statesWon[candidate];
-			newMayor = candidate;
+			stateHigh = votes[candidate][state];
+			stateWin = candidate;
 		}
 	}
-	newMayorChar = displayCandidate(newMayor);
-	printf("Candidate %c is the new mayor\m", newMayorChar);
-}
+	//return state winner
+	return stateWin;
+}//getStateWinner()
 
+/*
+Reads scores per state and returns the winning candidate
+*/
+void displayNewMayor(int stateResults[NO_STATES])
+{
+	int newMayor = -1;
+	int voteCountHigh = -1;
+
+	printf("Displaying state winners...\n");
+	for (int state = 0; state < NO_STATES; state++)
+	{
+		if (stateResults[state] > voteCountHigh)
+		{
+			newMayor = state;
+			voteCountHigh = stateResults[state];
+		}
+	}
+	printf("Candidate %c has been elected mayor!\n", displayCandidate(newMayor));
+}//displayNewMayor()
+
+/*
+Displays candate as A/B/C/D based on int input
+*/
 char displayCandidate(int candidateNo)
 {
 	switch (candidateNo)
@@ -129,4 +134,4 @@ char displayCandidate(int candidateNo)
 	case 3: return 'D';
 		break;
 	}
-}
+}//displayCandidate()
