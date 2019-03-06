36e8451035f8ca3aa37e599c775b9e7b4a1469a4
EOIN WILKIE <G00363134@gmit.ie>
2019-03-06 12:29:53 +0000

    +small changes to lab6

--- Lab6vs15_LinkedList/Main.c
+++ Lab6vs15_LinkedList/Main.c
@@ -7,25 +7,73 @@ typedef struct node {
 }node;
 
 //Add an element to the end of the list.
-int add();
+void add(struct node *node, int value);
 //Add an element to the top of the list.
-int addStart();
+void addStart(struct node *node, int value);
 //Display the contents of the list.
-int display();
+void display(struct node *node);
 //Delete an element from the list.
-int remove();
+void remove(struct node *node, int value);
 //Display the length of the list.
-int size();
+void size();
 //Search the list for a user specified integer.
-int search();
+void search(struct node *node, int value);
 /*new element*/
-int createLinkedList();
-int newIndex();
+void createLinkedList();
+void newIndex();
 
 void main() {
 
 	struct node *head;
-	head = (struct node *)malloc(sizeof(struct node));
-	head->next = NULL;
-	head->value = 5;
+
+	//tests
+	/*
+	add(head, 5);
+	display(head);
+	search(head, 1);
+	remove(head, 1);
+	add(head, 1);
+	search(head, 1);
+	size();
+	display(head);
+	remove(head, 1);
+	size();
+	*/
+}
+
+
+//Add an element to the end of the list.
+void add(struct node *node, int value) {
+	/*
+	node = (struct node *)malloc(sizeof(struct node));
+	node->next = NULL;
+	node->value = 5;
+	*/
+}//add()
+//Add an element to the top of the list.
+void addStart(struct node *node, int value) {
+
+}
+//Display the contents of the list.
+void display(struct node *node) {
+
+}
+//Delete an element from the list.
+void remove(struct node *node, int value) {
+
+}
+//Display the length of the list.
+void size() {
+
+}
+//Search the list for a user specified integer.
+void search(struct node *node, int value) {
+
+}
+/*new element*/
+void createLinkedList() {
+
+}
+void newIndex() {
+
 }
